90d30c9ace13819ba2c18923a95e4309926cf68d
orsoFra <francesco.corso@mail.polimi.it>
2022-05-14 00:52:08 +0200

    added getter methods with id parameter(for testing: /api/method?_id=number)

--- server/api.js
+++ server/api.js
@@ -24,14 +24,58 @@ app.get('/poi', async (req, res) => {
       images: element.Images,
     })
   }
-  console.log(result)
+  //console.log(result)
   return res.json(filtered)
 })
 
+//get poi from id
+app.get('/poi:id', async (req, res) => {
+  const _id = +req.params.id
+  const data = await dbData
+  const result = await data.PointOfInterest.findOne({
+    where:{ _id },
+    include: { model: data.Image },
+  })
+  const filtered = []
+  for (const element of result) {
+    filtered.push({
+      name: element.name,
+      description: element.description,
+      longitute: element.longitude,
+      latitude: element.latitude,
+      images: element.Images,
+    })
+  }
+  //console.log(result)
+  return res.json(filtered)
+})
+
+
 // Get all Itineraries
 app.get('/itinerary', async (req, res) => {
+  
   const data = await dbData
   const result = await data.Itinerary.findAll({
+    include: { model: data.PointOfInterest },
+  })
+  const filtered = []
+  for (const element of result) {
+    filtered.push({
+      name: element.name,
+      overview: element.overview,
+      poi: element.PointOfInteres,
+    })
+  }
+  // console.log(result)
+  return res.json(filtered)
+})
+
+// Get  Itinerary from id
+app.get('/itinerary:id', async (req, res) => {
+  const _id = +req.params.id
+  const data = await dbData
+  const result = await data.Itinerary.findOne({
+    where: { _id },
     include: { model: data.PointOfInterest },
   })
   const filtered = []
@@ -59,7 +103,27 @@ app.get('/service', async (req, res) => {
       adress: element.address,
     })
   }
-  console.log(filtered)
+  //console.log(filtered)
+  return res.json(filtered)
+})
+
+// Get  Service from id
+app.get('/service:id', async (req, res) => {
+  const _id = +req.params.id
+  const data = await dbData
+  const result = await data.Service.findOne({
+    where: {_id},
+  })
+  const filtered = []
+  for (const element of result) {
+    filtered.push({
+      name: element.name,
+      phone: element.phone,
+      email: element.email,
+      adress: element.address,
+    })
+  }
+  //console.log(filtered)
   return res.json(filtered)
 })
 
@@ -67,6 +131,26 @@ app.get('/service', async (req, res) => {
 app.get('/event', async (req, res) => {
   const data = await dbData
   const result = await data.Event.findAll()
+  const filtered = []
+  for (const element of result) {
+    filtered.push({
+      name: element.name,
+      overvuew: element.overview,
+      startDate: element.startDate,
+      endDate: element.endDate,
+      cost: element.cost,
+    })
+  }
+  //console.log(filtered)
+  return res.json(filtered)
+})
+//Get event from id
+app.get('/event:id', async (req, res) => {
+  const _id = +req.params.id
+  const data = await dbData
+  const result = await data.Event.findOne({
+    where:{ _id},
+  })
   const filtered = []
   for (const element of result) {
     filtered.push({
