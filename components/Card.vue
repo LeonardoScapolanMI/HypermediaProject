a9808a55c380508e4eb3a1db2af8b8bf088109bc
LeonardoScapolanMI <leonardo.scapolan@mail.polimi.it>
2022-05-17 11:17:36 +0200

    removed vue tutorial and solved bugs with header and footer

---
+++ components/Card.vue
@@ -0,0 +1,24 @@
+<template>
+  <div @click="SendClickEvent">
+    <img src=imageURL alt=imageCaption />
+    <h5>{{ title }}</h5>
+    <text> {{ description }} </text>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ItemCard',
+  props: {
+    imageURL: { type: String, required: true, },
+    imageCaption: { type: String, required: true, },
+    title: { type: String, required: true, },
+    description: { type: String, required: true, },
+  },
+  methods:{
+    SendClickEvent(){
+      this.$emit('click-event')
+    }
+  }
+}
+</script>
