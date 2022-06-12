ced541bacdccd7597b33911a2ed9b11f0dfbbaf7
Leonardo Scapolan <79846644+LeonardoScapolanMI@users.noreply.github.com>
2022-06-12 17:53:30 +0200

    Improved footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,20 @@
 <template>
-  <div>
+  <div class="flex-wrapper">
     <TheHeader />
     <Nuxt />
-    <TheFooter />
+    <TheFooter class="footer" />
   </div>
 </template>
+
+<style>
+.flex-wrapper {
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+  justify-content: flex-start;
+}
+
+.footer {
+  margin-top: auto;
+}
+</style>
