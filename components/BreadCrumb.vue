434e2ea1dba310b740f771682afd6a80f819ec9c
Leonardo Scapolan <79846644+LeonardoScapolanMI@users.noreply.github.com>
2022-06-27 12:40:49 +0200

    Removed package

--- components/BreadCrumb.vue
+++ components/BreadCrumb.vue
@@ -29,7 +29,6 @@
 </template>
 
 <script>
-const titleCase = require('ap-style-title-case')
 export default {
   props: {
     title: {
@@ -50,7 +49,7 @@ export default {
         const match = this.$router.match(path)
         if (match.name !== null) {
           crumbs.push({
-            title: titleCase(param.replace(/-/g, ' ')),
+            title: param.replace(/-/g, ' ').toUpperCase(),
             ...match,
           })
         }
