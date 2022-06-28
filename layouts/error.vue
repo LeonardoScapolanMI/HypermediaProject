ecef31ebbbdb465dd7cf0f75fb9b431ee3df5fba
orsoFra <francesco.corso@mail.polimi.it>
2022-06-28 23:23:15 +0200

    create error page

---
+++ layouts/error.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="__nuxt-error-page">
+    <div class="error">
+      <div class="title">La pagina cercata non esiste</div>
+      <hr>
+      <p v-if="statusCode === 404" class="description">
+        <a v-if="typeof $route === 'undefined'" class="error-link" href="/"></a>
+        <NuxtLink v-else class="error-link" to="/">Torna alla HOME</NuxtLink>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NuxtError',
+  props: {
+    error: {
+      type: Object,
+      default: null
+    }
+  },
+  computed: {
+    statusCode () {
+      return (this.error && this.error.statusCode) || 500
+    },
+    message () {
+      return this.error.message || 'Error'
+    }
+  },
+  head () {
+    return {
+      title: this.message,
+      meta: [
+        {
+          name: 'viewport',
+          content: 'width=device-width,initial-scale=1.0,minimum-scale=1.0'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+.__nuxt-error-page {
+  background: linear-gradient(var(--dark), var(--dark)), url("/images/home/welcome.jpg") center ;
+  background-size: cover;
+  height: 465px;
+}
+
+.__nuxt-error-page .title {
+  font-size: 30px;
+  margin-top: 200px;
+  padding: 0px;
+  color: var(--green);
+  background-color: var(--beige);
+}
+
+.__nuxt-error-page .description {
+  background-color: var(--beige);
+  text-align: center;
+  font-size: 20px;
+  font-weight: bold;
+}
+
+.__nuxt-error-page a {
+  color: var(--green);
+  text-decoration: none;
+}
+
+.__nuxt-error-page hr {
+  background-color: var(--green);
+  margin: 0px;
+}
+
+</style>
