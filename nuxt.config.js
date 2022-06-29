9b5057fc1e4e2b91b26e38c15117829ec6ccbdb6
Leonardo Scapolan <79846644+LeonardoScapolanMI@users.noreply.github.com>
2022-06-29 18:52:28 +0200

    solved a bug with api base url

--- nuxt.config.js
+++ nuxt.config.js
@@ -71,7 +71,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'https://https://hyper4hypermediaproject.herokuapp.com/',
+    baseURL: 'https://hyper4hypermediaproject.herokuapp.com/',
   },
 
   serverMiddleware: [
