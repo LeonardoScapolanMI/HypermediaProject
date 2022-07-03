e851eb3a721c361a319f0cad5f58d6ebb191e0cd
Francesco <fracala19@gmail.com>
2022-07-03 22:47:12 +0200

    inserito nuova immagine header

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,7 +1,9 @@
 <template>
   <nav class="navbar navbar-expand-lg sticky-top">
     <nuxt-link id="logo" to="/" tabindex="0">
-      <img id="icon" src="/images/firenze.jpg" alt="HOME" />
+    <div class="img">
+      <img id="icon" src="/images/firenze.png" alt="HOME" />
+    </div>
     </nuxt-link>
 
     <button tabindex="0"
@@ -85,6 +87,7 @@ nav li {
 #icon {
   border: 0;
   width: 90px;
+  height: 80px;
 }
 
 nav a {
@@ -109,4 +112,11 @@ nav a:hover, nav a:focus {
 .navbar-toggler-icon {
   background-image: url("data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='rgb(65, 69, 53)' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 8h24M4 16h24M4 24h24'/%3E%3C/svg%3E");
 }
+
+.img {
+  width:100%;
+  padding:0;
+
+}
+
 </style>
